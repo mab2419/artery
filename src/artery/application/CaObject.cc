a2998c1abae3032c287ea56002fa210121f90f82
Max Bauder <Maximilian.Bauder@carissma.eu>
2022-02-23 10:47:07 +0100

    CAM Detailauswertung hinzugefügt

--- src/artery/application/CaObject.cc
+++ src/artery/application/CaObject.cc
@@ -91,4 +91,101 @@ protected:
 
 Register_ResultFilter("camGenerationDeltaTime", CamGenerationDeltaTimeResultFilter)
 
+class CamVelocityResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto velocity = cam->asn1()->cam.camParameters.highFrequencyContainer.choice.basicVehicleContainerHighFrequency.speed.speedValue;
+            fire(this, t, velocity, details);
+        }
+    }
+};
+//Velocity wird in cm/s ausgegeben
+Register_ResultFilter("camVelocity", CamVelocityResultFilter)
+
+class CamRefPoslongResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto RefPoslong = cam->asn1()->cam.camParameters.basicContainer.referencePosition.longitude;
+            fire(this, t, RefPoslong, details);
+        }
+    }
+};
+//Microdegree nochmal mit 10 multipliziert --> Umrechnung in x,y in Traci Doku nachschauen
+Register_ResultFilter("camRefPoslong", CamRefPoslongResultFilter)
+
+class CamRefPoslatResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto RefPoslat = cam->asn1()->cam.camParameters.basicContainer.referencePosition.latitude;
+            fire(this, t, RefPoslat, details);
+        }
+    }
+};
+//Microdegree nochmal mit 10 multipliziert --> Umrechnung in x,y in Traci Doku nachschauen
+Register_ResultFilter("camRefPoslat", CamRefPoslatResultFilter)
+
+class CamHeadingResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto Heading = cam->asn1()->cam.camParameters.highFrequencyContainer.choice.basicVehicleContainerHighFrequency.heading.headingValue;
+            fire(this, t, Heading, details);
+        }
+    }
+};
+//decidegree, Winkel bezüglich Nordvektor
+Register_ResultFilter("camHeading", CamHeadingResultFilter)
+
+class CamLongAccelResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto LongAccel = cam->asn1()->cam.camParameters.highFrequencyContainer.choice.basicVehicleContainerHighFrequency.longitudinalAcceleration.longitudinalAccelerationValue;
+            fire(this, t, LongAccel, details);
+        }
+    }
+};
+// Einheit m/s²
+Register_ResultFilter("camLongAccel", CamLongAccelResultFilter)
+
+class CamCurvatureResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto Curvature = cam->asn1()->cam.camParameters.highFrequencyContainer.choice.basicVehicleContainerHighFrequency.curvature.curvatureValue;
+            fire(this, t, Curvature, details);
+        }
+    }
+};
+//1/meter *10000
+Register_ResultFilter("camCurvature", CamCurvatureResultFilter)
+
+class CamYawRateResultFilter : public cObjectResultFilter
+{
+protected:
+    void receiveSignal(cResultFilter* prev, simtime_t_cref t, cObject* object, cObject* details) override
+    {
+        if (auto cam = dynamic_cast<CaObject*>(object)) {
+            const auto YawRate = cam->asn1()->cam.camParameters.highFrequencyContainer.choice.basicVehicleContainerHighFrequency.yawRate.yawRateValue;
+            fire(this, t, YawRate, details);
+        }
+    }
+};
+// °/s*100
+Register_ResultFilter("camYawRate", CamYawRateResultFilter)
 } // namespace artery
